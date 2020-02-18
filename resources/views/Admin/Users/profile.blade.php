bd00ee90de5dbd745404aabb0a0f28af9fd3d115
Utsab <pokharelutsab567@gmail.com>
2020-02-18 17:05:51 +0545

    profile

--- resources/views/Admin/Users/profile.blade.php
+++ resources/views/Admin/Users/profile.blade.php
@@ -30,8 +30,9 @@
                                              class="img-responsive" alt=""></div>
                                 </div>
                                 <div class="profile-usertitle">
-                                    <div class="profile-usertitle-name">{{Auth::user()->name}}</div>
+                                    <div class="profile-usertitle-name">{{Auth::user()->name}}</div>                           
                                     <div class="profile-usertitle-job"> {{strtoupper(Auth::user()->roles->name)}}</div>
+                                    <div class="profile-usertitle-email">Email : {{(Auth::user()->email)}} </div>
                                     <div class="profile-usertitle-job">Joined Date/Time
                                         : {{strtoupper(Auth::user()->created_at)}}</div>
 
@@ -64,7 +65,7 @@
                                 <!-- END SIDEBAR BUTTONS -->
                             </div>
                         </div>
-                        <div class="card">
+                        <!-- <div class="card">
                             <div class="card-head card-topline-aqua">
                                 <header>Performance</header>
                             </div>
@@ -90,40 +91,47 @@
                                     </li>
                                 </ul>
                             </div>
-                        </div>
+                        </div> -->
 
-
-                    </div>
-                    <!-- END BEGIN PROFILE SIDEBAR -->
-                    <!-- BEGIN PROFILE CONTENT -->
-                    <div class="profile-content">
-                        <div class="row">
-                            <div class="col-md-12">
                                 <div class="card">
                                     <div class="card-head card-topline-aqua">
-                                        <header>About Me</header>
+                                        <header>Personal Details</header>
                                     </div>
                                     <div class="card-body padding height-9">
-                                        <div class="profile-desc">
+                                        <!-- <div class="profile-desc">
                                             Hello I am Samyam a web and user interface designer. I love to work
                                             with the application interface and
                                             the web elements.
-                                        </div>
-                                        <ul class="list-group list-group-bordered">
+                                        </div> -->
+                                        <ul class="list-group list-group-unbordered">
+                                            <li class="list-group-item">
+                                                <b>Current Address</b>
+                                                <div class="profile-desc-item pull-right"></div>
+                                            </li>
+                                            <li class="list-group-item">
+                                                <b>Permanent Address</b>
+                                                <div class="profile-desc-item pull-right">dasfgh</div>
+                                            </li>
                                             <li class="list-group-item">
                                                 <b>Gender </b>
                                                 <div class="profile-desc-item pull-right">{{strtoupper(Auth::user()->gender)}}</div>
                                             </li>
+                                            
                                             <li class="list-group-item">
-                                                <b>Contact</b>
-                                                <div class="profile-desc-item pull-right">88888888</div>
+                                                <b>Department</b>
+                                                <div class="profile-desc-item pull-right">{{Auth::user()->department->name}}</div>
+                                            </li>
+
+                                            <li class="list-group-item">
+                                                <b>Contact No.</b>
+                                                <div class="profile-desc-item pull-right">cvhjkl</div>
                                             </li>
                                             <li class="list-group-item">
-                                                <b>Skills</b>
-                                                <div class="profile-desc-item pull-right">Java,Spring</div>
+                                                <b>Date of Birth</b>
+                                                <div class="profile-desc-item pull-right">wreiu</div>
                                             </li>
                                         </ul>
-                                        <div class="row list-separated profile  ">
+                                        <!-- <div class="row list-separated profile  ">
                                             <div class="col-md-4 col-sm-4 col-6">
                                                 <div class="uppercase profile-stat-title"> 37</div>
                                                 <div class="uppercase profile-stat-text"> Projects</div>
@@ -136,56 +144,100 @@
                                                 <div class="uppercase profile-stat-title"> 61</div>
                                                 <div class="uppercase profile-stat-text"> Uploads</div>
                                             </div>
-                                        </div>
+                                        </div> -->
+                                    </div>
+                                </div>
+
+                    </div>
+                    <!-- END BEGIN PROFILE SIDEBAR -->
+                    <!-- BEGIN PROFILE CONTENT -->
+                    <div class="profile-content">
+                        <div class="row">
+                            <div class="col-md-12">
+                                
+                                <div class="card">
+                                    <div class="card-head card-topline-aqua">
+                                        <header>Education Details</header>
+                                    </div>
+                                    <div class="card-body padding height-9">
+                                        
+                                        <ul class="list-group list-group-unbordered">
+                                            <li class="list-group-item">
+                                                <b>Degree </b>
+                                                <div class="profile-desc-item pull-right">asdfghjk</div>
+                                            </li>
+                                            
+                                            <li class="list-group-item">
+                                                <b>Board</b>
+                                                <div class="profile-desc-item pull-right">hjygrj</div>
+                                            </li>
+                                            <li class="list-group-item">
+                                                <b>Faculty</b>
+                                                <div class="profile-desc-item pull-right">hjygrj</div>
+                                            </li>
+                                            <li class="list-group-item">
+                                                <b>Institute Name</b>
+                                                <div class="profile-desc-item pull-right">hjygrj</div>
+                                            </li>
+                                            <li class="list-group-item">
+                                                <b>Institute Address</b>
+                                                <div class="profile-desc-item pull-right">hjygrj</div>
+                                            </li>
+                                            <li class="list-group-item">
+                                                <b>Passed Year</b>
+                                                <div class="profile-desc-item pull-right">hjygrj</div>
+                                            </li>
+                                            <li class="list-group-item">
+                                                <b>Division</b>
+                                                <div class="profile-desc-item pull-right">hjygrj</div>
+                                            </li>
+                                        </ul>
+                                        
                                     </div>
                                 </div>
 
                                 <div class="card">
                                     <div class="card-head card-topline-aqua">
-                                        <header>Work Progress</header>
+                                        <header>Work Details</header>
                                     </div>
                                     <div class="card-body padding height-9">
-                                        <div class="work-monitor work-progress">
-                                            <div class="states">
-                                                <div class="info">
-                                                    <div class="desc pull-left">Operations</div>
-                                                    <div class="percent pull-right">80%</div>
+                                        
+                                        <ul class="list-group list-group-unbordered">
+                                            <li class="list-group-item">
+                                             <b> Work Experience</b>
+                                                <div class="profile-desc">
+                                                Hello I am Samyam a web and user interface designer. I love to work
+                                                with the application interface and
+                                                the web elements.
                                                 </div>
-                                                <div class="progress progress-xs">
-                                                    <div class="progress-bar progress-bar-danger progress-bar-striped active"
-                                                         role="progressbar" aria-valuenow="40" aria-valuemin="0"
-                                                         aria-valuemax="100" style="width: 70%">
-                                                        <span class="sr-only">80% </span>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="states">
-                                                <div class="info">
-                                                    <div class="desc pull-left">Consultation</div>
-                                                    <div class="percent pull-right">55%</div>
-                                                </div>
-                                                <div class="progress progress-xs">
-                                                    <div class="progress-bar progress-bar-success progress-bar-striped active"
-                                                         role="progressbar" aria-valuenow="40" aria-valuemin="0"
-                                                         aria-valuemax="100" style="width: 45%">
-                                                        <span class="sr-only">55% </span>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="states">
-                                                <div class="info">
-                                                    <div class="desc pull-left">Support</div>
-                                                    <div class="percent pull-right">20%</div>
-                                                </div>
-                                                <div class="progress progress-xs">
-                                                    <div class="progress-bar progress-bar-warning progress-bar-striped active"
-                                                         role="progressbar" aria-valuenow="40" aria-valuemin="0"
-                                                         aria-valuemax="100" style="width: 35%">
-                                                        <span class="sr-only">20% </span>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
+                                            </li>
+                                            <li class="list-group-item">
+                                                <b>Profession </b>
+                                                <div class="profile-desc-item pull-right">asdfghjk</div>
+                                            </li>
+                                                                                                                                    
+                                            <li class="list-group-item">
+                                                <b>Organization Name</b>
+                                                <div class="profile-desc-item pull-right">hjygrj</div>
+                                            </li>
+                                            <li class="list-group-item">
+                                                <b>Organization Address</b>
+                                                <div class="profile-desc-item pull-right">hjygrj</div>
+                                            </li>
+                                            <li class="list-group-item">
+                                                <b>Organization Phone</b>
+                                                <div class="profile-desc-item pull-right">hjygrj</div>
+                                            </li>
+                                            <li class="list-group-item">
+                                                <b>Organization PAN No.</b>
+                                                <div class="profile-desc-item pull-right">hjygrj</div>
+                                            </li>
+                                            <li class="list-group-item">
+                                                <b>Document</b>
+                                                <div class="profile-desc-item pull-right">hjygrj</div>
+                                            </li>
+                                        </ul>
+                                        
                                     </div>
                                 </div>
                             </div>
