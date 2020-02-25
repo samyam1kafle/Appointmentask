65bbe91f8b9acd1573f212111808c4d6edb25f2b
Utsab <pokharelutsab567@gmail.com>
2020-02-25 14:07:46 +0545

    some changes

--- resources/views/Admin/Booking/create.blade.php
+++ resources/views/Admin/Booking/create.blade.php
@@ -32,16 +32,16 @@
                                    autocomplete="on">
                                    {{csrf_field()}}    
 
-                                   {{--<div class="form-group row  margin-top-20">--}}
-                                        {{--<label class="control-label col-md-3">Name--}}
-                                            {{--<span class="required"> * </span>--}}
-                                        {{--</label>--}}
-                                        {{--<div class="col-md-8">--}}
-                                            {{--<div class="input-icon right">--}}
-                                                {{--<i class="fa"></i>--}}
-                                                {{--<input type="text" class="form-control" value="" name="name"/></div>--}}
-                                        {{--</div>--}}
-                                    {{--</div>--}}
+                                   <div class="form-group row  margin-top-20">
+                                        <label class="control-label col-md-3">Booking Purpose
+                                            <span class="required"> * </span>
+                                        </label>
+                                        <div class="col-md-8">
+                                            <div class="input-icon right">
+                                                <i class="fa"></i>
+                                                <input type="text" class="form-control" value="" name="name"/></div>
+                                        </div>
+                                    </div>
 
                                     <div class="form-group row">
                                         <label class="control-label col-md-3">User
